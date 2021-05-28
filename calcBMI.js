1edd9dad147696501940e3688fbc07865584f7f3
ashu02-git <shuhei.abe1107@gmail.com>
2021-05-28 15:58:36 +0900

    肥満度バグ修正

--- calcBMI.js
+++ calcBMI.js
@@ -3,45 +3,38 @@ class BmiCaliculator {
         this.name = name;
         this.height = height;
         this.weight = weight;
-    }
 
-    // BMI算出
-    calcBmi() {
-        let bmi = this.weight / ((this.height/100)**2);
-        return(bmi.toFixed(2));
-    }
-    
-    // 適正体重算出
-    standardWeight() {
-        let standard = ((this.height * 0.01) ** 2) * 22;
-        return(standard.toFixed(2));
+        // BMI算出
+        this.bmi = (this.weight / ((this.height/100)**2)).toFixed(2);
+        // 適正体重算出
+        this.standard = (((this.height * 0.01) ** 2) * 22).toFixed(2);
     }
 
     // 最適体重と比較
-    compareWeiht() {
-        let compare = this.weight - this.standardWeight();
+    compareWeight() {
+        let compare = this.weight - this.standard;
         return(compare.toFixed(2));
     }
 
     // bmi結果振り分け
-    evaluation() {
-        let result;
-        
-        if (this.bmi < 18.5) {
-            result = 0; 
-        } else if (18.5 <= this.bmi < 25) {
-            result = 1;
-        } else if (25 <= this.bmi < 30){
-            result = 2;
-        } else if (30 <= this.bmi < 45){
-            result = 3;
-        } else if ( this.bmi){
-            result = 4;
+    level() {
+        let level;
+        const resultBmi = this.bmi;
+        if (resultBmi < 18.5) {
+            level = 0; 
+        } else if (18.5 <= resultBmi && resultBmi < 25) {
+            level = 1;
+        } else if (25 <= resultBmi && resultBmi < 30){
+            level = 2;
+        } else if (30 <= resultBmi && resultBmi < 35){
+            level = 3;
+        } else if (35 <= resultBmi && resultBmi < 40){
+            level = 4;
         } else {
-            resultlt = 5;
+            level =5;
         }
 
-        return result;
+        return level;
     }
     
     // 肥満度出力
@@ -56,31 +49,18 @@ class BmiCaliculator {
         ]
         return textFatness[result]
     }
-    // BMI該当範囲出力
-    printRange(result) {
-        const range = [
-            '18.5未満',
-            '18.5~25未満',
-            '25~30未満',
-            '30~35未満',
-            '35~40未満',
-            '40以上'
-        ]
-        return range[result];
-    }
 
     // データ出力
     displayResult() {
         console.log('名前：'+ this.name);
         console.log('身長：' + this.height + 'cm');
         console.log('体重：' + this.weight + 'kg');
-        console.log('範囲：' + this.printRange(this.evaluation()));
-        console.log('肥満度：'+ this.printFatness(this.evaluation()));
-        console.log('適正体重：' + this.standardWeight() + 'kg');
-        console.log('適正体重と比較：' + this.compareWeiht() + 'kg');
-        console.log('BMI：' + this.calcBmi());
+        console.log('肥満度：'+ this.printFatness(this.level()));
+        console.log('適正体重：' + this.standard + 'kg');
+        console.log('適正体重と比較：' + this.compareWeight() + 'kg');
+        console.log('BMI：' + this.bmi);
     }
 }
 
-let satoBmi = new BmiCaliculator('sato', 190, 1);
+let satoBmi = new BmiCaliculator('sato', 180, 130);
 satoBmi.displayResult();
